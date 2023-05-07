9fa0b75858f1a0d20a10a45481d73d77754b7d72
Talip Sütçü <89219306+mtsutcu@users.noreply.github.com>
2023-05-07 16:00:00 +0300

    Test commit2

---
+++ mytestlibrary/src/main/java/com/neonapps/mytestlibrary/TestClass.kt
@@ -0,0 +1,18 @@
+package com.neonapps.mytestlibrary
+
+class TestClass {
+
+    companion object{
+        fun test1(s:String){
+            println(s)
+        }
+
+        val test1object = "Test1"
+    }
+
+    fun test2(s:String){
+        println("test2: $s")
+    }
+
+    val test2object = "Test2"
+}
