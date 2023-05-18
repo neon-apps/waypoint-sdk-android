449114ab4b32cb68a8096a9256593c36d12ff798
Talip Sütçü <89219306+mtsutcu@users.noreply.github.com>
2023-05-18 18:50:05 +0300

    Custom Adaptive Decoration Bug Fixing

--- waypointsdk/src/main/java/com/neonapps/waypointsdk/utils/CustomAdaptiveDecoration.kt
+++ waypointsdk/src/main/java/com/neonapps/waypointsdk/utils/CustomAdaptiveDecoration.kt
@@ -4,14 +4,31 @@ import android.graphics.Rect
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
-class CustomAdaptiveDecoration(private val spanCount: Int = 1, private val spacingHorizontal: Int, private val spacingVertical: Int,private val includeEdge: Boolean = true) :
+class CustomAdaptiveDecoration(
+    private val spanCount: Int = 1,
+    private val spacingHorizontal: Int,
+    private val spacingVertical: Int,
+    private val itemCount: Int,
+) :
     RecyclerView.ItemDecoration() {
 
-    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        super.getItemOffsets(outRect, view, parent, state)
         val position = parent.getChildAdapterPosition(view)
         val column = position % spanCount
-
-        if (includeEdge) {
+        if (spanCount == 1) {
+            outRect.top = spacingVertical
+            if (position == itemCount - 1) {
+                outRect.bottom = spacingVertical
+            }
+            outRect.left = spacingHorizontal
+            outRect.right = spacingHorizontal
+        } else {
             outRect.left = spacingVertical - column * spacingVertical / spanCount
             outRect.right = (column + 1) * spacingVertical / spanCount
 
@@ -19,12 +36,6 @@ class CustomAdaptiveDecoration(private val spanCount: Int = 1, private val spaci
                 outRect.top = spacingHorizontal
             }
             outRect.bottom = spacingHorizontal
-        } else {
-            outRect.left = column * spacingHorizontal / spanCount
-            outRect.right = spacingHorizontal - (column + 1) * spacingHorizontal / spanCount
-            if (position >= spanCount) {
-                outRect.top = spacingHorizontal
-            }
         }
     }
 }
