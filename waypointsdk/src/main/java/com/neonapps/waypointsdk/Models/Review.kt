970be3fd5d3a90d82fca031a63afaaa52986c417
Talip Sütçü <89219306+mtsutcu@users.noreply.github.com>
2023-05-23 16:40:40 +0300

    Shimmer Items, Light Grey Button Background and Review Model Update

--- waypointsdk/src/main/java/com/neonapps/waypointsdk/Models/Review.kt
+++ waypointsdk/src/main/java/com/neonapps/waypointsdk/Models/Review.kt
@@ -1,7 +1,11 @@
 package com.neonapps.waypointsdk.Models
 
+import com.neonapps.waypointsdk.Models.Users.Customer
+
 data class Review(
     var rate : Double,
-    var comment : String
+    var comment : String,
+    var customer: Customer,
+    var restaurant: Restaurant
 ) {
 }
